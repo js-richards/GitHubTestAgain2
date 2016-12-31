9368dbf2c668750cff11cd3fd3782ba071aeaf6d
Jeffrey <jeffrey.richards.406@gmail.com>
2016-12-30 19:46:24 -0700

    Initial commit

---
+++ app/src/main/java/com/example/jsrichards/githubtest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.jsrichards.githubtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+//Really want this to work. Attempt one at saving to GitHub for loading onto the desktop
+
+//This is after pressing Ctrl+S to save. Android Studio said it was saved as a file in the local git repo
